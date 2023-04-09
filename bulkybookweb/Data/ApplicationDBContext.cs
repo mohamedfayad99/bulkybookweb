63d4123df2c6d15b4293b5a8ea593df456015a4c
mohamedfayad99 <mofayad992001@gmail.com>
2023-04-09 15:26:35 +0200

    Add project files.

---
+++ bulkybookweb/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using bulkybookweb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace bulkybookweb.Data
+{
+    public class ApplicationDBContext :DbContext
+    {
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
